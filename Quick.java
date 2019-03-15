e7fa60d532ae1978c84d7406c6f84e7a1dc3220d
Kevin Chen <kchen01@stuy.edu>
2019-03-14 23:10:55 -0400

    not sure if it works yet, but tested on my 2 tiny arrays

--- Quick.java
+++ Quick.java
@@ -11,12 +11,22 @@ public class Quick {
     int timer= start - end;
     start += 1;
     int temp2 = 0;
-    while (start<= end) {
+    int temp4 = 0;
+    while (start <= end) {
       if(data[start] > pivot) {
           temp2 = data[start];
           data[start] = data[end];
           data[end] = temp2;
           end -= 1;
+      }
+      else if(data[start] == pivot) {
+          temp4 += 1;
+          if (temp4 % 2 == 1) {
+          temp2 = data[start];
+          data[start] = data[end];
+          data[end] = temp2;
+          end -= 1;
+        }
       }
       else {
         start += 1;
@@ -25,11 +35,44 @@ public class Quick {
       int temp3 = data[start - 1];
       data[start - 1] = pivot;
       data[inital] = temp3;
-      System.out.println(pivot);
-      System.out.println(printarray(data));
-
       return start - 1;
     }
+    public static int partitionSort ( int[] data, int start,int pivot, int end){
+      Random a = new Random();
+      int select = pivot;
+      int inital = start;
+      int temp = data[start];
+      data[start] = pivot;
+      data[select] = temp;
+      int timer= start - end;
+      start += 1;
+      int temp2 = 0;
+      int temp4 = 0;
+      while (start <= end) {
+        if(data[start] > pivot) {
+            temp2 = data[start];
+            data[start] = data[end];
+            data[end] = temp2;
+            end -= 1;
+        }
+        else if(data[start] == pivot) {
+            temp4 += 1;
+            if (temp4 % 2 == 1) {
+            temp2 = data[start];
+            data[start] = data[end];
+            data[end] = temp2;
+            end -= 1;
+          }
+        }
+        else {
+          start += 1;
+        }
+        }
+        int temp3 = data[start - 1];
+        data[start - 1] = pivot;
+        data[inital] = temp3;
+        return start - 1;
+      }
   public static String printarray(int[] a) {
     String output = "";
     for(int i = 0; i < a.length; i++) {
@@ -48,20 +91,45 @@ public class Quick {
         return data[temp];
       }
       if (temp < k) {
-        lower = temp;
+        lower = temp + 1;
       }
       if (temp > k) {
-        higher = temp;
+        higher = temp - 1;
       }
     }
     return 0;
    }
+   public static void quickSort(int []data) {
+
+     quickSorthelper(data, 0, data.length - 1);
+   }
+   public static void quickSorthelper(int[] data, int start, int end) {
+     if(start >= end) {
+       return;
+     }
+     int split = partition(data,start,end);
+     quickSorthelper(data,start,split- 1);
+     quickSorthelper(data,split + 1, end);
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 
    public static void main(String[] args) {
-    // int[] a = {746, 134, 1989, 1268, 1574, 103, 1999, 34, 743, 1252, 1446, 978, 1930, 1331, 159, 37, 533, 1722, 1061, 1868, 923, 1658, 670, 66, 1135, 43, 1569, 190, 1227, 1699, 644, 719, 1170, 1683, 817, 1625, 1344, 1494, 370, 1116, 1918, 438, 1841, 1911, 1277, 1422, 1023, 1563, 799, 51};
-     for(int i = 0; i < 1; i++) {
-        int[] b = {10, 7, 6,4 ,12}; //, 8, 4 , 3 , 56 , 12 , 6};
-       System.out.println(partition(b,0,b.length - 1));
+     int[] b = {10, 10, 6,4 ,12}; //, 8, 4 , 3 , 56 , 12 , 6};
+     int[] a = {134, 134, 1989, 1268, 1574, 103, 1999, 34, 743, 1252, 1446, 978, 1930, 1331, 159, 37, 533, 1722, 1061, 1868, 923, 1658, 670, 66, 1135, 43, 1569, 190, 1227, 1699, 644, 719, 1170, 1683, 817, 1625, 1344, 1494, 370, 1116, 1918, 438, 1841, 1911, 1277, 1422, 1023, 1563, 799, 51};
+     for(int i = 0; i < 1;i++) {
+       quickSort(a);
+       System.out.println(printarray(a));
      }
    }
 
